4a472368648ee9b2b44f6d70d6eec28494f31eaf
Maksims Piļščikovs <m.pilscikov@gmail.com>
2021-03-30 21:03:28 +0300

    Initial commit 2

--- main.py
+++ main.py
@@ -3,3 +3,10 @@
 # izveidot git repozitoriju lokāli
 # izveidot remote repozitoriju (GitHub)
 # pievienot remote
+# another comment
+
+# master -> main
+# lokāli master
+# remote main
+
+# pirms pirmā commit neviens branch neeksistē
